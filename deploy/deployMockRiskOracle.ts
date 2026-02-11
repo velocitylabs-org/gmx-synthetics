1acd69efb03496fe0cd74fded7cd47f74990d73b
Jonatan del Valle <delvallejonatan@gmail.com>
2026-02-11 08:04:46 +0100

    Add localhost to MockRiskOracle deploy skip list
    Required for `--network localhost` deployments used in keeper E2E testing.

--- deploy/deployMockRiskOracle.ts
+++ deploy/deployMockRiskOracle.ts
@@ -6,36 +6,7 @@ const func = createDeployFunction({
   getDeployArgs: async () => {
     const [wallet] = await ethers.getSigners();
     const initialSenders = [wallet.address];
-    const initialUpdateTypes = [
-      "maxLongTokenPoolAmount",
-      "maxShortTokenPoolAmount",
-      "maxLongTokenPoolUsdForDeposit",
-      "maxShortTokenPoolUsdForDeposit",
-      "maxOpenInterestForLongs",
-      "maxOpenInterestForShorts",
-      "positivePositionImpactFactor",
-      "negativePositionImpactFactor",
-      "positionImpactExponentFactor",
-      "positiveSwapImpactFactor",
-      "negativeSwapImpactFactor",
-      "swapImpactExponentFactor",
-      "fundingIncreaseFactorPerSecond",
-      "fundingDecreaseFactorPerSecond",
-      "minFundingFactorPerSecond",
-      "maxFundingFactorPerSecond",
-      "borrowingFactorForLongs",
-      "borrowingFactorForShorts",
-      "borrowingExponentFactorForLongs",
-      "borrowingExponentFactorForShorts",
-      "reserveFactorLongs",
-      "reserveFactorShorts",
-      "openInterestReserveFactorLongs",
-      "openInterestReserveFactorShorts",
-      "optimalUsageFactor",
-      "baseBorrowingFactor",
-      "aboveOptimalUsageBorrowingFactor",
-      "maxPnlFactorForTradersLongs",
-    ];
+    const initialUpdateTypes = ["maxLongTokenPoolAmount", "maxShortTokenPoolAmount", "maxLongTokenPoolUsdForDeposit", "maxShortTokenPoolUsdForDeposit", "maxOpenInterestForLongs", "maxOpenInterestForShorts", "positivePositionImpactFactor", "negativePositionImpactFactor", "positionImpactExponentFactor", "positiveSwapImpactFactor", "negativeSwapImpactFactor", "swapImpactExponentFactor", "fundingIncreaseFactorPerSecond", "fundingDecreaseFactorPerSecond", "minFundingFactorPerSecond", "maxFundingFactorPerSecond", "borrowingFactorForLongs", "borrowingFactorForShorts", "borrowingExponentFactorForLongs", "borrowingExponentFactorForShorts", "reserveFactorLongs", "reserveFactorShorts", "openInterestReserveFactorLongs", "openInterestReserveFactorShorts", "optimalUsageFactor", "baseBorrowingFactor", "aboveOptimalUsageBorrowingFactor", "maxPnlFactorForTradersLongs"];
     return [initialSenders, initialUpdateTypes];
   },
 });
