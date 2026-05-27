24145763d7a4b3b6d9baae1257903325611dd338
Nuno Alexandre <hi@nunoalexandre.com>
2026-05-27 13:36:46 +0200

    Doc validation preset

--- scripts/configs/presets/validate.ts
+++ scripts/configs/presets/validate.ts
@@ -1,6 +1,14 @@
 import { FeatureFlags } from "./types";
 
+/**
+ * The ENV Vars Preset for when we want to run the Contract Redaction Validations.
+ * Only `RUN_FEATURE_VALIDATIONS` is set to true.
+ */
 export const validatePreset: FeatureFlags = {
+  // We only want to run the validations
+  RUN_FEATURE_VALIDATIONS: true,
+
+  // The rest is false
   RUN_ORDER_FEATURE_REDACTION: false,
   RUN_POOL_RISK_GUARDS: false,
   RUN_INVARIANT_VALIDATIONS: false,
@@ -9,5 +17,4 @@ export const validatePreset: FeatureFlags = {
   RUN_SUBACCOUNT_FEATURE: false,
   RUN_GASLESS_FEATURE: false,
   RUN_ATOMIC_WITHDRAWAL_FEATURE: false,
-  RUN_FEATURE_VALIDATIONS: true,
 };
